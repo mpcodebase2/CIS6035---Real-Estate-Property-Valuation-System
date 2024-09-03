134b885b9573dbd9e19dcf01f24213cfca2cf939
mpcodebase2 <125908081+mpcodebase2@users.noreply.github.com>
2024-09-03 22:47:59 +0530

    Crud app integration

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'P347word@$#'
-    MYSQL_DB = 'user_auth'
-    MYSQL_CURSORCLASS = 'DictCursor'
