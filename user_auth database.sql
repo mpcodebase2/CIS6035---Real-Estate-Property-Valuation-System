2373b23d70c0c99450a3b9166ff8147abb594121
mpcodebase2 <125908081+mpcodebase2@users.noreply.github.com>
2024-09-11 18:29:33 +0530

    Delete user_auth database.sql

--- user_auth database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE user_auth;
-
-USE user_auth;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
